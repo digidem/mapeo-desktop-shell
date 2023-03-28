c9fb9757dbee26d8207e8375a0d27313349d3a8d
lightlii <32508751+lightlii@users.noreply.github.com>
2023-03-28 14:51:43 +0200

    feat: add Language switcher

--- src/renderer/src/components/LanguageSwitch/index.tsx
+++ src/renderer/src/components/LanguageSwitch/index.tsx
@@ -1,7 +1,9 @@
 import { FormControl, MenuItem, Select, Stack } from '@mui/material'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { useContext, useState } from 'react'
 import { IntlSwitchConext } from '../../components/IntlProvider'
 import languagesConfig from './languages.json'
+import { Row } from '../LayoutComponents'
 
 export const LanguageSwitcher = () => {
   const [open, setOpen] = useState(false)
@@ -12,14 +14,34 @@ export const LanguageSwitcher = () => {
 
   return (
     <FormControl sx={{ m: 1, minWidth: 120, cursor: 'pointer' }} size="small" variant="standard">
-      <Stack direction="row">
+      <Row>
         <Select
           open={open}
           onClose={handleClose}
           onOpen={handleOpen}
           value={lang}
           IconComponent={() => null}
-          renderValue={() => lang.toUpperCase()}
+          sx={{
+            '& .MuiInput-input.MuiSelect-select': {
+              paddingRight: 1,
+            },
+            ':after': {
+              borderBottom: 'none',
+            },
+            ':before': {
+              borderBottom: 'none',
+            },
+            ':hover&:before': {
+              borderBottom: 'none',
+            },
+          }}
+          renderValue={() => (
+            <>
+              <Row>
+                <ExpandMoreIcon /> {lang.toUpperCase()}
+              </Row>
+            </>
+          )}
         >
           <MenuItem value="en" onClick={() => setLang('en')}>
             {languagesConfig['en']?.nativeName}
@@ -28,7 +50,7 @@ export const LanguageSwitcher = () => {
             {languagesConfig['es']?.nativeName}
           </MenuItem>
         </Select>
-      </Stack>
+      </Row>
     </FormControl>
   )
 }
---
+++ src/renderer/src/components/LayoutComponents/index.tsx
@@ -0,0 +1,4 @@
+import { Stack, StackProps } from '@mui/material'
+
+export const Row = (props: StackProps) => <Stack direction="row" {...props} />
+export const Column = (props: StackProps) => <Stack direction="column" {...props} />
