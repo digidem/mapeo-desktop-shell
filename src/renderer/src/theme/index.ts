9856811ddd2cb0d451dfb1e06d92064284d3d4ea
lightlii <32508751+lightlii@users.noreply.github.com>
2023-03-29 11:36:45 +0200

    feat: add theme.ts for keeping colours etc centralised

---
+++ src/renderer/src/theme/index.ts
@@ -0,0 +1,15 @@
+export const MAPEO_BLUE = '#0066FF' as React.CSSProperties['color']
+export const MAPEO_ORANGE = '#E86826' as React.CSSProperties['color']
+export const OFF_WHITE = '#F6F6F6' as React.CSSProperties['color']
+export const WHITE = '#FFFFFF' as React.CSSProperties['color']
+export const OFF_BLACK = '#333333' as React.CSSProperties['color']
+export const MIDNIGHT_BLUE = '#000033' as React.CSSProperties['color']
+
+export const theme = {
+  blue: MAPEO_BLUE,
+  midnightBlue: MIDNIGHT_BLUE,
+  orange: MAPEO_ORANGE,
+  background: OFF_WHITE,
+  foreground: OFF_BLACK,
+  white: WHITE,
+}
