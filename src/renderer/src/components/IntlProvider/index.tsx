773c0723646420f79d158bc4acfc6eefd40719b8
lightlii <32508751+lightlii@users.noreply.github.com>
2023-03-30 17:32:42 +0200

    chore: update IntlProvider component to filter from available languages

--- src/renderer/src/components/IntlProvider/index.tsx
+++ src/renderer/src/components/IntlProvider/index.tsx
@@ -1,44 +1,85 @@
-import { IntlConfig, IntlProvider as ReactIntlProvider } from 'react-intl'
+import { IntlProvider as ReactIntlProvider } from 'react-intl'
 import { createContext, Dispatch, ReactNode, SetStateAction, useEffect, useState } from 'react'
+import translations from '../../../translations/messages.json'
+import languages from '../../../../common/config/languages.json'
 
-import * as es from '../../../translations/es.json'
-import * as en from '../../../translations/en.json'
+const DEFAULT_LOCALE = 'en'
 
-const DEFAULT_LOCALE = 'es'
-
-type Translations = {
-  es: IntlConfig['messages']
-  en: IntlConfig['messages']
+type AvailableLocales = keyof typeof translations
+type SupportedLanguageLocales = keyof typeof languages
+interface LanguageName {
+  /** IETF BCP 47 langauge tag with region code. */
+  locale: SupportedLanguageLocales
+  /** Localized name for language */
+  nativeName: string
+  /** English name for language */
+  englishName: string
 }
 
-const translations: Translations = { es, en }
+const translatedLocales = Object.keys(translations) as Array<AvailableLocales>
 
-type AvailableLocales = keyof typeof translations | undefined
+export const supportedLanguages: LanguageName[] = translatedLocales
+  .filter((locale) => {
+    const hasAtLeastOneTranslatedString = Object.keys(translations[locale]).length > 0
+    // This will show a typescript error if the language name does not exist
+    const hasTranslatedLanguageName = languages[locale]
+    if (!hasTranslatedLanguageName) {
+      console.warn(
+        `Locale "${locale}" is not available in Mapeo because we do not have
+a language name and translations in \`src/frontend/languages.json\``,
+      )
+    }
+    return hasAtLeastOneTranslatedString && hasTranslatedLanguageName
+  })
+  .map((locale) => ({
+    locale,
+    ...languages[locale],
+  }))
+  .sort((a, b) => {
+    return a.englishName.localeCompare(b.englishName)
+  })
 
 const isTranslation = (langugage?: string): langugage is AvailableLocales => {
   if (!langugage) return false
   return Object.keys(translations).includes(langugage)
 }
 
-type IntlSetContextType = Readonly<[string, Dispatch<SetStateAction<AvailableLocales>>]>
+type IntlSetContextType = Readonly<[string, Dispatch<SetStateAction<AvailableLocales | undefined>>]>
 
 export const IntlSwitchConext = createContext<IntlSetContextType>([DEFAULT_LOCALE, (): void => {}])
 
+const getSupportedLocale = (locale: SupportedLanguageLocales): keyof typeof languages | undefined => {
+  if (supportedLanguages.find((lang) => lang.locale === locale)) return locale as keyof typeof languages
+  const nonRegionalLocale = locale.split('-')[0]
+  if (supportedLanguages.find(({ locale }) => locale === nonRegionalLocale))
+    return nonRegionalLocale as keyof typeof languages
+}
+
 export const IntlProvider = ({ children }: { children: ReactNode }): JSX.Element | null => {
-  const [lang, setLang] = useState<AvailableLocales>()
+  const [appLocale, setAppLocale] = useState<AvailableLocales>()
+
+  const locale = appLocale || appLocale ? getSupportedLocale(appLocale) : DEFAULT_LOCALE
+
+  const languageCode = locale ? locale.split('-')[0] : null
+
+  // Add fallbacks for non-regional locales (e.g. "en" for "en-GB")
+  const localeMessages = {
+    ...translations[languageCode as AvailableLocales],
+    ...(translations[locale as AvailableLocales] || {}),
+  }
 
   useEffect(() => {
     const setupApp = async (): Promise<void> => {
       const locale = await window.api.getLocale()
-      setLang(isTranslation(locale) ? locale : DEFAULT_LOCALE)
+      setAppLocale(isTranslation(locale) ? locale : DEFAULT_LOCALE)
     }
 
     setupApp()
   }, [])
 
-  return lang ? (
-    <ReactIntlProvider messages={translations[lang]} locale={lang} defaultLocale={DEFAULT_LOCALE}>
-      <IntlSwitchConext.Provider value={[lang, setLang]}>{children}</IntlSwitchConext.Provider>
+  return appLocale ? (
+    <ReactIntlProvider messages={localeMessages} locale={appLocale} defaultLocale={DEFAULT_LOCALE}>
+      <IntlSwitchConext.Provider value={[appLocale, setAppLocale]}>{children}</IntlSwitchConext.Provider>
     </ReactIntlProvider>
   ) : null
 }
