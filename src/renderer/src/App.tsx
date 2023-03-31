cb84bba0809e73a80e3bb507aa67e764a6485fe6
lightlii <32508751+lightlii@users.noreply.github.com>
2023-03-31 16:40:46 +0200

    feat: add theme and theme provider

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,5 @@
 import { IntlProvider } from './components/IntlProvider'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import { RouteComponentProps, Router } from '@reach/router'
 import { IndexView as Index } from './views/Index'
@@ -7,6 +8,7 @@ import { OFF_BLACK as black, OFF_WHITE as white } from './theme'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const IndexView = (_props: RouteComponentProps) => <Index />
+const MigrationView = (_props: RouteComponentProps) => <Migration />
 
 function App(): JSX.Element | null {
   return (
--- src/renderer/src/theme/index.ts
+++ src/renderer/src/theme/index.ts
@@ -1,15 +1,68 @@
-export const MAPEO_BLUE = '#0066FF' as React.CSSProperties['color']
-export const MAPEO_ORANGE = '#E86826' as React.CSSProperties['color']
-export const OFF_WHITE = '#F6F6F6' as React.CSSProperties['color']
-export const WHITE = '#FFFFFF' as React.CSSProperties['color']
-export const OFF_BLACK = '#333333' as React.CSSProperties['color']
-export const MIDNIGHT_BLUE = '#000033' as React.CSSProperties['color']
+import { createTheme } from '@mui/material'
 
-export const theme = {
-  blue: MAPEO_BLUE,
-  midnightBlue: MIDNIGHT_BLUE,
-  orange: MAPEO_ORANGE,
+export const MAPEO_BLUE = '#0066FF'
+export const MAPEO_ORANGE = '#E86826'
+export const OFF_WHITE = '#F6F6F6'
+export const WHITE = '#FFFFFF'
+export const OFF_BLACK = '#333333'
+export const MIDNIGHT_BLUE = '#000033'
+export const WARNING_RED = '#D92222'
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    primary: React.CSSProperties['color']
+    background: React.CSSProperties['color']
+    foreground: React.CSSProperties['color']
+    blueDark: React.CSSProperties['color']
+    warningRed: React.CSSProperties['color']
+    black: React.CSSProperties['color']
+    orange: React.CSSProperties['color']
+    white: React.CSSProperties['color']
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    primary: React.CSSProperties['color']
+    background: React.CSSProperties['color']
+    foreground: React.CSSProperties['color']
+    blueDark: React.CSSProperties['color']
+    warningRed: React.CSSProperties['color']
+    black: React.CSSProperties['color']
+    orange: React.CSSProperties['color']
+    white: React.CSSProperties['color']
+  }
+}
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Rubik',
+      'Roboto',
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
   background: OFF_WHITE,
   foreground: OFF_BLACK,
+  blueDark: MIDNIGHT_BLUE,
+  primary: MAPEO_BLUE,
+  warningRed: WARNING_RED,
+  black: OFF_BLACK,
+  orange: MAPEO_ORANGE,
   white: WHITE,
-}
+  palette: {
+    primary: { main: MAPEO_BLUE },
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+        // disableElevation: true,
+      },
+    },
+  },
+})
