630d808bf9af16fa8ec2df3f40a63e5c0fa5de51
lightlii <32508751+lightlii@users.noreply.github.com>
2023-03-31 16:40:46 +0200

    feat: add theme and theme provider

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,17 +1,26 @@
 import { IntlProvider } from './components/IntlProvider'
+import CssBaseline from '@mui/material/CssBaseline'
 
 import { RouteComponentProps, Router } from '@reach/router'
 import { IndexView as Index } from './views/Index'
+import { MigrationView as Migration } from './views/Migration'
+import { ThemeProvider } from '@mui/material'
+import { theme } from './theme'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const IndexView = (_props: RouteComponentProps) => <Index />
+const MigrationView = (_props: RouteComponentProps) => <Migration />
 
 function App(): JSX.Element | null {
   return (
     <IntlProvider>
-      <Router>
-        <IndexView path="/" />
-      </Router>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <MigrationView path="/" />
+          <IndexView path="/" />
+        </Router>
+      </ThemeProvider>
     </IntlProvider>
   )
 }
