22dc241cf9fe1b56aad2f1032938452a9f73daca
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-03 14:27:04 +0200

    chore: further styling to init screen

--- src/renderer/src/views/Migration/index.tsx
+++ src/renderer/src/views/Migration/index.tsx
@@ -1,5 +1,6 @@
-import { Button, Card, CardContent, CardHeader, Checkbox, Typography, useTheme } from '@mui/material'
+import { Card, CardContent, CardHeader, Checkbox, Typography, useTheme } from '@mui/material'
 import CategoryIcon from '@mui/icons-material/Category'
+import { Button } from '@renderer/components/Button'
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera'
 import { Column, Row } from '@renderer/components/LayoutComponents'
 import { DefaultLayout } from '@renderer/layouts/default'
@@ -8,44 +9,64 @@ import { defineMessages, useIntl } from 'react-intl'
 import { appStrings } from '../../../../common/config/messages'
 import { Logo } from '../SplashScreen/logo'
 import { BoldSpan, Contents } from './styles'
+import { useRef } from 'react'
 
 const PROJECT_KEY = '2023R**********'
 const CONFIG_ADDRESS = 'mapeoconf.v3'
-const OBSERVATIONS = 66
-const IMAGES = 14
 
 export const MigrationView = () => {
   const intl = useIntl()
   const theme = useTheme()
+  const observations = useRef(Math.floor(Math.random() * 100))
+  const images = useRef(Math.floor(Math.random() * 100))
 
   const appTitle = intl.formatMessage(appStrings.appTitle)
 
   return (
     <DefaultLayout themeVarient="dark">
       <Row sx={{ height: '100vh' }}>
-        <Column sx={{ bgcolor: theme.background, flex: 1, padding: '6em 8em' }} spacing={6}>
-          <span>
-            <Typography variant="h1">{intl.formatMessage(messages.welcomeTitle)}</Typography>
-            <Typography variant="h1">{appTitle}</Typography>
-          </span>
-          <Typography variant="h2">{intl.formatMessage(messages.migratePreviousTitle)}</Typography>
-          <Column spacing={4}>
-            <Typography variant="body1">{intl.formatMessage(messages.migratePreviousInstruction)}</Typography>
-            <Column component="ul" spacing={2}>
-              <CheckItem message={intl.formatMessage(messages.migrateInstruction1)} />
-              <CheckItem message={intl.formatMessage(messages.migrateInstruction2, { appTitle })} />
-            </Column>
-            <DetailsCard
-              projectKey={PROJECT_KEY}
-              configAddress={CONFIG_ADDRESS}
-              observations={OBSERVATIONS}
-              images={IMAGES}
-            ></DetailsCard>
-            <Row justifyContent={'space-between'}>
-              <Button sx={{ color: theme.warningRed }}>{intl.formatMessage(messages.skipMigration)}</Button>
-              <Button>{intl.formatMessage(messages.migrate)}</Button>
-            </Row>
+        <Column
+          sx={{ bgcolor: theme.background, flex: 1, padding: '6em 8em 3em 8em' }}
+          justifyContent={'space-between'}
+        >
+          <Column spacing={3}>
+            <span>
+              <Typography variant="h1">{intl.formatMessage(messages.welcomeTitle)}</Typography>
+              <Typography variant="h1">{appTitle}</Typography>
+            </span>
+            <Typography variant="h2">{intl.formatMessage(messages.migratePreviousTitle)}</Typography>
           </Column>
+          <Column spacing={6}>
+            <Column spacing={3}>
+              <Typography variant="body1">
+                {intl.formatMessage(messages.migratePreviousInstruction)}
+              </Typography>
+              <Column component="ul" spacing={2}>
+                <CheckItem message={intl.formatMessage(messages.migrateInstruction1)} />
+                <CheckItem message={intl.formatMessage(messages.migrateInstruction2, { appTitle })} />
+              </Column>
+            </Column>
+          </Column>
+          <DetailsCard
+            projectKey={PROJECT_KEY}
+            configAddress={CONFIG_ADDRESS}
+            observations={observations.current}
+            images={images.current}
+          />
+          <Row justifyContent="space-between" alignItems="flex-start">
+            {/* Buttons currently do nothing */}
+            <Button onClick={() => null} variant="text" sx={{ color: theme.warningRed }}>
+              {intl.formatMessage(messages.skipMigration)}
+            </Button>
+            <Column alignItems="flex-end" spacing={1}>
+              <Button onClick={() => null} variant="contained" disableElevation>
+                {intl.formatMessage(messages.migrate)}
+              </Button>
+              <Typography variant="caption" component="label">
+                {intl.formatMessage(messages.migrateInfo)}
+              </Typography>
+            </Column>
+          </Row>
         </Column>
 
         <Contents
@@ -106,7 +127,7 @@ const DetailsCard = ({ projectKey, configAddress, observations, images }: Detail
   const intl = useIntl()
 
   return (
-    <Card>
+    <Card sx={{ width: '85%' }}>
       <CardHeader
         sx={{ backgroundColor: theme.grey.light }}
         title={<DetailsCardHeader projectKey={projectKey} configAddress={configAddress} />}
