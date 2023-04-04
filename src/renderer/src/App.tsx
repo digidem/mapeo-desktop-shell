1cb32583092df45e84a0580c41636e19b52c5632
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-04 15:47:28 +0200

    chore: set global CSS colours from theme variables

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,8 @@ import { IntlProvider } from './components/IntlProvider'
 
 import { RouteComponentProps, Router } from '@reach/router'
 import { IndexView as Index } from './views/Index'
+import GlobalStyles from '@mui/material/GlobalStyles'
+import { OFF_BLACK as black, OFF_WHITE as white } from './theme'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const IndexView = (_props: RouteComponentProps) => <Index />
@@ -9,6 +11,12 @@ const IndexView = (_props: RouteComponentProps) => <Index />
 function App(): JSX.Element | null {
   return (
     <IntlProvider>
+      <GlobalStyles
+        styles={{
+          color: black,
+          backgroundColor: white,
+        }}
+      ></GlobalStyles>
       <Router>
         <IndexView path="/" />
       </Router>
