9b52c6682079207b467cb02849a2cef00ee374d4
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-11 18:48:09 +0200

    Revert "feat: add navigation screen"
    This reverts commit a4dffc6a51ad2801b079cc626e58d60fbcc927e8.

--- src/renderer/src/layouts/default/index.tsx
+++ src/renderer/src/layouts/default/index.tsx
@@ -1,29 +1,17 @@
 import { ReactNode } from 'react'
 import { LanguageSwitcher } from '../../components/LanguageSwitch'
-import { Box, SxProps } from '@mui/material'
-import MenuOpenIcon from '@mui/icons-material/MenuOpen'
-import { Link, useLocation } from 'react-router-dom'
 
 export const DefaultLayout = ({
   children,
   themeVarient = 'light',
-  sx,
 }: {
   children: ReactNode
   themeVarient?: 'light' | 'dark'
-  sx?: SxProps
 }) => {
-  const location = useLocation()
-  console.log({ location })
   return (
-    <Box minHeight="100vh" minWidth="100vw" sx={sx}>
-      {location.pathname !== '/' ? (
-        <Link to="/">
-          <MenuOpenIcon sx={{ position: 'fixed', top: 1, left: 4, cursor: 'pointer' }} />
-        </Link>
-      ) : null}
+    <div>
       <LanguageSwitcher themeVarient={themeVarient} />
       {children}
-    </Box>
+    </div>
   )
 }
--- src/renderer/src/views/Index/index.tsx
+++ src/renderer/src/views/Index/index.tsx
@@ -1,13 +1,5 @@
-import { useTheme } from '@mui/material'
-import { Link } from 'react-router-dom'
 import { DefaultLayout } from '@renderer/layouts/default'
 
 export const IndexView = () => {
-  const theme = useTheme()
-
-  return (
-    <DefaultLayout sx={{ backgroundColor: theme.blue.mid, padding: 8 }} themeVarient="dark">
-      <Link to="/init-migration">Test migration</Link>
-    </DefaultLayout>
-  )
+  return <DefaultLayout>Test text</DefaultLayout>
 }
