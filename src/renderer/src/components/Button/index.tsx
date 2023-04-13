2536f012e3be298ea0dfa2ff2253ad3c2adae5dc
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-13 12:37:04 +0200

    chore: simplified button component (removed loading + disabled states)

--- src/renderer/src/components/Button/index.tsx
+++ src/renderer/src/components/Button/index.tsx
@@ -1,15 +1,9 @@
 /* eslint-disable no-nested-ternary */
-import { ButtonProps, CircularProgress, SvgIconTypeMap } from '@mui/material'
-import { OverridableComponent } from '@mui/material/OverridableComponent'
+import { ButtonProps } from '@mui/material'
 import { StyledButton } from './styles'
-
-type IconType = OverridableComponent<SvgIconTypeMap> | null
 
 type ButtonPropTypesHelper = ButtonProps & {
   children: React.ReactNode
-  icon?: IconType
-  loading?: boolean
-  disabled?: boolean
   fullWidth?: boolean
   variant: ButtonProps['variant']
 }
@@ -29,9 +23,6 @@ type ButtonPropTypes = ButtonPropTypesOnSubmit | ButtonPropTypesOnClick
 export const Button = ({
   children,
   onSubmit,
-  loading,
-  disabled,
-  icon = null,
   fullWidth = false,
   color = 'secondary',
   variant = 'contained',
@@ -53,18 +44,10 @@ export const Button = ({
       fontWeight: 600,
       ...sx,
     }}
-    endIcon={<ButtonIcon loading={loading} icon={icon} />}
     onSubmit={onSubmit}
-    disabled={disabled || loading}
     disableElevation
     {...rest}
   >
     {children}
   </StyledButton>
 )
-
-const ButtonIcon = ({ loading, icon: Icon }: { loading?: boolean; icon: IconType }) => {
-  if (loading) return <CircularProgress sx={{ color: 'white' }} size="1em" />
-
-  return Icon ? <Icon /> : null
-}
