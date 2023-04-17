226b327310e02ffc7be8fbbdf998b3bb18fdf683
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-17 16:15:21 +0200

    chore: extract two column layout into generic layout component

---
+++ src/renderer/src/layouts/Onboarding/index.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react'
+import { DefaultLayout } from '../default'
+import { Column, Row } from '@renderer/components/LayoutComponents'
+import { styled, useTheme } from '@mui/material'
+import { Logo } from '@renderer/components/Logo'
+
+export const OnboardingLayout = ({ children }: { children: ReactNode }) => {
+  const theme = useTheme()
+  return (
+    <DefaultLayout langBackgroundVarient="dark">
+      <Row sx={{ height: '100vh' }}>
+        <Column sx={{ bgcolor: theme.background, flex: 1, padding: '6em 8em 3em 8em' }}>{children}</Column>
+
+        <Contents
+          sx={{
+            height: '100%',
+            flex: 1,
+            backgroundColor: theme.primary,
+            flexDirection: 'column',
+            padding: '6em 8em',
+          }}
+        >
+          <Logo />
+        </Contents>
+      </Row>
+    </DefaultLayout>
+  )
+}
+
+export const Contents = styled(Column)`
+  justify-content: center;
+  align-items: center;
+`
--- src/renderer/src/views/Migration/styles.ts
+++ src/renderer/src/views/Migration/styles.ts
@@ -1,10 +1,4 @@
 import styled from '@emotion/styled'
-import { Column } from '@renderer/components/LayoutComponents'
-
-export const Contents = styled(Column)`
-  justify-content: center;
-  align-items: center;
-`
 
 export const BoldSpan = styled('span')`
   font-weight: 700;
