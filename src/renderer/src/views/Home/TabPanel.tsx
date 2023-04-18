4dd3660be16afb5b300d492f4dc3e6f39b7d54fb
Andrew Chou <andrewchou@fastmail.com>
2023-04-18 10:26:37 -0400

    fix styling syntax in TabPanel

--- src/renderer/src/views/Home/TabPanel.tsx
+++ src/renderer/src/views/Home/TabPanel.tsx
@@ -12,7 +12,7 @@ const Container = styled.div<{ hide: boolean }>(
   right: 0;
   bottom: 0;
   left: 0;
-  visibility: ${hide ? 'hidden' : undefined};
+  visibility: ${hide ? 'hidden' : 'visible'};
 `,
 )
 
