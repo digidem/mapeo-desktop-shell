dea3198f7909ecbb7ff37684a84bcba3be79ac21
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-18 13:08:01 -0700

    chore: add memoized note

--- src/renderer/src/hooks/useMapeoDeviceList.ts
+++ src/renderer/src/hooks/useMapeoDeviceList.ts
@@ -1,9 +1,12 @@
 import { mapeoDeviceStore } from './stores/mapeoDeviceStore'
-
+/**
+ * This should only be used to display a list. When dealing with a singular device, use `useMapeDevice` as that is memoized. This component will update all its children when any component in the list is updated.
+ *
+ */
 export const useMapeoDeviceList = () => {
   const devices = mapeoDeviceStore((store) => store.devices)
   return {
     members: Object.values(devices).filter((val) => val.role !== 'waitingInvite'),
     nonMembers: Object.values(devices).filter((val) => val.role === 'waitingInvite'),
-  }
+  } as const
 }
