b4baaf5fa653290eb6e6daae4edabc9c4714817a
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-18 14:49:22 +0200

    chore: add /migration-complete route to router

--- src/renderer/src/components/Router/index.tsx
+++ src/renderer/src/components/Router/index.tsx
@@ -2,6 +2,7 @@ import { Routes, HashRouter, Route } from 'react-router-dom'
 import { IndexView } from '../../views/Index'
 import { MigrationView } from '../../views/Migration'
 import { MigratingProjectView } from '@renderer/views/MigratingProject'
+import { MigrationCompleteView } from '@renderer/views/MigrationComplete'
 
 export const Router = () => (
   <HashRouter>
@@ -9,6 +10,7 @@ export const Router = () => (
       <Route path="/" element={<IndexView />} />
       <Route path="/init-migration" element={<MigrationView />} />
       <Route path="/migrating-project" element={<MigratingProjectView />} />
+      <Route path="/migration-complete" element={<MigrationCompleteView />} />
     </Routes>
   </HashRouter>
 )
