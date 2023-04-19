2eb545e70be0af386ad4add4cb08aeb60550e44c
Andrew Chou <andrewchou@fastmail.com>
2023-04-19 15:13:26 -0400

    adjust application of css reset and global styles

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import '../assets/index.css'
+import { CssBaseline, GlobalStyles } from '@mui/material'
+
+import { OFF_BLACK as black, OFF_WHITE as white } from './theme'
 import App from './App'
+
+const GLOBAL_STYLES = {
+  color: black,
+  backgroundColor: white,
+  fontFamily: `font-family: Rubik, Roboto, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif;`,
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <CssBaseline />
+    <GlobalStyles styles={GLOBAL_STYLES} />
     <App />
   </React.StrictMode>,
 )
