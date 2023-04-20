98fc402e283b13b7931c5d013a8f13d1f3eb2ab7
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-20 12:16:50 +0200

    chore: remove nav menu icon for now

--- src/renderer/src/layouts/default/index.tsx
+++ src/renderer/src/layouts/default/index.tsx
@@ -1,9 +1,6 @@
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 import { LanguageSwitcher } from '../../components/LanguageSwitch'
-import { Box, SxProps, Tooltip } from '@mui/material'
-import MenuOpenIcon from '@mui/icons-material/MenuOpen'
-import { Link, useLocation } from 'react-router-dom'
-import { defineMessages, useIntl } from 'react-intl'
+import { Box, SxProps } from '@mui/material'
 
 export const DefaultLayout = ({
   children,
@@ -14,50 +11,10 @@ export const DefaultLayout = ({
   langBackgroundVarient?: 'light' | 'dark'
   sx?: SxProps
 }) => {
-  const location = useLocation()
-  const intl = useIntl()
-  const [isHovered, setIsHovered] = useState(false)
-
-  const handleClose = () => {
-    setIsHovered(false)
-  }
-
-  const handleOpen = () => {
-    setIsHovered(true)
-  }
-
   return (
     <Box minHeight="100vh" minWidth="100vw" sx={sx}>
-      {location.pathname !== '/' ? (
-        <Tooltip
-          title={intl.formatMessage(messages.exitTesting)}
-          enterDelay={0}
-          leaveDelay={200}
-          placement="right"
-          open={isHovered}
-          onClose={handleClose}
-          onOpen={handleOpen}
-        >
-          <Box
-            sx={{ position: 'fixed', top: 1, left: 4, m: 1 }}
-            onMouseOver={handleOpen}
-            onMouseOut={handleClose}
-          >
-            <Link to="/">
-              <MenuOpenIcon sx={{ cursor: 'pointer' }} />
-            </Link>
-          </Box>
-        </Tooltip>
-      ) : null}
       <LanguageSwitcher langBackgroundVarient={langBackgroundVarient} />
       {children}
     </Box>
   )
 }
-
-const messages = defineMessages({
-  exitTesting: {
-    id: 'common.defaultLayout.exitTesting',
-    defaultMessage: 'Exit testing and return to navigation',
-  },
-})
