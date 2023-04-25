526f5cca0e730698db87afd986b7ed48c4a9d61d
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-25 15:46:54 -0700

    chore: fix rebase

--- src/renderer/src/components/Router/index.tsx
+++ src/renderer/src/components/Router/index.tsx
@@ -1,18 +1,20 @@
 import { Routes, HashRouter, Route } from 'react-router-dom'
 import { IndexView } from '../../views/Index'
 import { MigrationView } from '../../views/Migration'
+import { MigrationNoDataView } from '../../views/MigrationNoData'
 import { MigratingProjectView } from '@renderer/views/MigratingProject'
+import { MigrationCompleteView } from '@renderer/views/MigrationComplete'
 import { Home } from '@renderer/views/Home'
-import { MigrationNoDataView } from '@renderer/views/MigrationNoData'
 
 export const Router = () => (
   <HashRouter>
     <Routes>
       <Route path="/" element={<IndexView />} />
+      <Route path="/migration-no-data" element={<MigrationNoDataView />} />
       <Route path="/init-migration" element={<MigrationView />} />
       <Route path="/migrating-project" element={<MigratingProjectView />} />
+      <Route path="/migration-complete" element={<MigrationCompleteView />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/migration-no-data" element={<MigrationNoDataView />}></Route>
     </Routes>
   </HashRouter>
 )
