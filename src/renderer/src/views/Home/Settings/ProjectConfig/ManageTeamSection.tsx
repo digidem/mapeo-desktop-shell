74919caa6831377e383db21367203fe19378cca9
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-25 15:44:53 -0700

    chore: rebase issues

--- src/renderer/src/views/Home/Settings/ProjectConfig/ManageTeamSection.tsx
+++ src/renderer/src/views/Home/Settings/ProjectConfig/ManageTeamSection.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react'
 import { defineMessages, useIntl } from 'react-intl'
-import styled from '@emotion/styled'
 import { useTheme } from '@mui/material'
 import PersonAddIcon from '@mui/icons-material/PersonAddAltOutlined'
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts'
+import GroupIcon from '@mui/icons-material/Group'
 import { Column, Row } from '@renderer/components/LayoutComponents'
-import { OFF_BLACK } from '@renderer/theme'
 import { spacing } from '@renderer/theme/spacing'
 import { MemberDevice } from '@renderer/hooks/stores/mapeoDeviceStore'
 import { useMapeoDeviceMembers } from '@renderer/hooks/useMapeoMembersList'
 
-import desktopImageUrl from '../../../../../assets/desktop.png'
-import mobileImageUrl from '../../../../../assets/mobile.png'
 import { PressableText } from './PressableText'
 import { Text } from './Text'
 import { Button, ButtonText } from './Button'
-import { LeaveProjectModal } from '@renderer/components/LeaveProjectModal'
+import { PeerCard } from './PeerCard'
 
 const m = defineMessages({
   title: {
@@ -41,73 +39,35 @@ const m = defineMessages({
     id: 'views.Home.Settings.ProjectConfig.ManageTeamSection.leaveProject',
     defaultMessage: 'Leave Project',
   },
+  thisDevice: {
+    id: 'views.Home.Settings.ProjectConfig.ManageTeamSection.thisDevice',
+    defaultMessage: 'This Device!',
+  },
 })
 
-const BlackSquarePlaceholder = styled.div(
-  ({ rotated }: { rotated?: boolean }) => `
-  width: 30px;
-  height: 30px;
-  background-color: ${OFF_BLACK};
-  margin-inline-end: ${spacing.medium};
-  ${rotated ? 'transform: rotate(45deg);' : undefined}
-`,
-)
-
-const MemberCard = ({
-  member,
-  openLeaveProjectModal,
-}: {
-  member: MemberDevice
-  openLeaveProjectModal: () => void
-}) => {
+const MemberCard = ({ member }: { member: MemberDevice }) => {
   const { formatMessage: t } = useIntl()
-  const theme = useTheme()
+
   return (
-    <Row
-      padding={spacing.large}
-      alignItems="center"
-      sx={{
-        maxWidth: '400px',
-        minWidth: '300px',
-        backgroundColor: theme.white,
-        borderRadius: '6px',
-        border: `1px solid ${theme.grey.light}`,
-      }}
-    >
-      <Column marginRight={spacing.medium}>
-        <img
-          src={member.deviceType === 'desktop' ? desktopImageUrl : mobileImageUrl}
-          style={{ display: 'block', maxWidth: '100%', width: '40px' }}
-        />
-      </Column>
-      <Column flex={1}>
-        <Row justifyContent="space-between">
-          <Text size="small" fontWeight="600">
-            {member.name}
-          </Text>
-          <Text size="small" color={theme.grey.main}>
-            {new Date(member.dateAdded).toLocaleDateString()}
-          </Text>
-        </Row>
-        <Row justifyContent="space-between">
-          <Text size="small" color={theme.grey.main}>
-            {member.isSelf ? 'This Device!' : member.deviceId}
-          </Text>
-          {member.isSelf && (
-            <PressableText destructive onClick={openLeaveProjectModal}>
-              {t(m.leaveProject)}
-            </PressableText>
-          )}
-        </Row>
-      </Column>
-    </Row>
+    <PeerCard
+      deviceType={member.deviceType}
+      title={member.name}
+      subtitle={member.isSelf ? t(m.thisDevice) : member.deviceId}
+      dateText={new Date(member.dateAdded).toLocaleDateString()}
+      pressableAction={
+        member.isSelf ? (
+          <PressableText destructive onClick={() => {}}>
+            {t(m.leaveProject)}
+          </PressableText>
+        ) : undefined
+      }
+    />
   )
 }
 
-export const ManageTeamSection = () => {
+export const ManageTeamSection = ({ onInviteClick }: { onInviteClick: () => void }) => {
   const { formatMessage: t } = useIntl()
   const theme = useTheme()
-  const [leaveProjectModalOpen, setLeaveProjectModalOpen] = React.useState(false)
 
   const members = useMapeoDeviceMembers()
 
@@ -127,72 +87,57 @@ export const ManageTeamSection = () => {
   }, [members])
 
   return (
-    <React.Fragment>
-      <Column padding={spacing.large} sx={{ backgroundColor: theme.background }} flex={1}>
-        <Column spacing={spacing.medium}>
-          <Column spacing={spacing.small}>
-            <Row>
-              <Text variant="h2" size="medium" fontWeight="600">
-                {t(m.title)}
-              </Text>
-            </Row>
-            <Row>
-              <Text size="medium" variant="body1">
-                {t(m.description)}
-              </Text>
-            </Row>
-          </Column>
+    <Column padding={spacing.large} sx={{ backgroundColor: theme.background }} flex={1}>
+      <Column spacing={spacing.medium}>
+        <Column spacing={spacing.small}>
           <Row>
-            <Button variant="outlined" onClick={() => {}}>
-              <Row spacing={spacing.small} alignItems="center">
-                <PersonAddIcon sx={{ fontSize: '18px' }} />
-                <ButtonText>{t(m.invite)}</ButtonText>
-              </Row>
-            </Button>
+            <Text variant="h2" size="medium" fontWeight="600">
+              {t(m.title)}
+            </Text>
+          </Row>
+          <Row>
+            <Text size="medium" variant="body1">
+              {t(m.description)}
+            </Text>
           </Row>
         </Column>
-        <Column paddingY={spacing.large} spacing={spacing.large}>
-          <Column spacing={spacing.large}>
-            <Row alignItems="center">
-              <BlackSquarePlaceholder />
-              <Text size="medium" fontWeight="600">
-                {t(m.coordinators)}
-              </Text>
+        <Row>
+          <Button variant="outlined" onClick={onInviteClick}>
+            <Row spacing={spacing.small} alignItems="center">
+              <PersonAddIcon sx={{ fontSize: '18px' }} />
+              <ButtonText>{t(m.invite)}</ButtonText>
             </Row>
-            <Column spacing={spacing.medium}>
-              {coordinators.map((c) => (
-                <MemberCard
-                  key={c.deviceId}
-                  member={c}
-                  openLeaveProjectModal={() => setLeaveProjectModalOpen(true)}
-                />
-              ))}
-            </Column>
+          </Button>
+        </Row>
+      </Column>
+      <Column paddingY={spacing.large} spacing={spacing.large}>
+        <Column spacing={spacing.large}>
+          <Row alignItems="center" spacing={spacing.small}>
+            <ManageAccountsIcon fontSize="large" htmlColor={theme.black} />
+            <Text size="medium" fontWeight="600">
+              {t(m.coordinators)}
+            </Text>
+          </Row>
+          <Column spacing={spacing.medium}>
+            {coordinators.map((c) => (
+              <MemberCard key={c.deviceId} member={c} />
+            ))}
           </Column>
-          <Column spacing={spacing.large}>
-            <Row alignItems="center">
-              <BlackSquarePlaceholder rotated />
-              <Text size="medium" fontWeight="600">
-                {t(m.participants)}
-              </Text>
-            </Row>
-            <Column spacing={spacing.medium}>
-              {participants.map((p) => (
-                <MemberCard
-                  key={p.deviceId}
-                  openLeaveProjectModal={() => setLeaveProjectModalOpen(true)}
-                  member={p}
-                />
-              ))}
-            </Column>
+        </Column>
+        <Column spacing={spacing.large}>
+          <Row alignItems="center" spacing={spacing.small}>
+            <GroupIcon fontSize="large" htmlColor={theme.black} />
+            <Text size="medium" fontWeight="600">
+              {t(m.participants)}
+            </Text>
+          </Row>
+          <Column spacing={spacing.medium}>
+            {participants.map((p) => (
+              <MemberCard key={p.deviceId} member={p} />
+            ))}
           </Column>
         </Column>
       </Column>
-      <LeaveProjectModal
-        closeModal={() => setLeaveProjectModalOpen(false)}
-        isOpen={leaveProjectModalOpen}
-        projectName="catapult"
-      />
-    </React.Fragment>
+    </Column>
   )
 }
--- src/renderer/src/views/Home/Settings/ProjectConfig/index.tsx
+++ src/renderer/src/views/Home/Settings/ProjectConfig/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react'
 import { defineMessages, useIntl } from 'react-intl'
 import { Box } from '@mui/material'
 import { Row } from '@renderer/components/LayoutComponents'
+import { JoinProjectModal } from '@renderer/components/JoinProjectModal'
 import { spacing } from '@renderer/theme/spacing'
 
 import { Button } from './Button'
 import { ConfigSection } from './ConfigSection'
 import { ManageTeamSection } from './ManageTeamSection'
 import { Text } from './Text'
+import { InviteDeviceModal } from './InviteDeviceModal'
 
 const m = defineMessages({
   title: {
@@ -20,7 +22,7 @@ const m = defineMessages({
   },
 })
 
-const HeaderSection = () => {
+const HeaderSection = ({ onJoinProject }: { onJoinProject: () => void }) => {
   const { formatMessage: t } = useIntl()
 
   return (
@@ -33,7 +35,7 @@ const HeaderSection = () => {
       <Text fontWeight="600" size="large" variant="h1">
         {t(m.title)}
       </Text>
-      <Button variant="outlined" onClick={() => {}}>
+      <Button variant="outlined" onClick={onJoinProject}>
         {t(m.joinProject)}
       </Button>
     </Row>
@@ -41,11 +43,16 @@ const HeaderSection = () => {
 }
 
 export const ProjectConfig = () => {
+  const [joinProjectModalOpen, setJoinProjectModalOpen] = React.useState(false)
+  const [inviteModalOpen, setInviteModalOpen] = React.useState(false)
+
   return (
     <Box display="flex" justifyContent="flex-start" flexDirection="column" flex={1}>
-      <HeaderSection />
+      <JoinProjectModal open={joinProjectModalOpen} onClose={() => setJoinProjectModalOpen(false)} />
+      <InviteDeviceModal open={inviteModalOpen} onClose={() => setInviteModalOpen(false)} />
+      <HeaderSection onJoinProject={() => setJoinProjectModalOpen(true)} />
       <ConfigSection />
-      <ManageTeamSection />
+      <ManageTeamSection onInviteClick={() => setInviteModalOpen(true)} />
     </Box>
   )
 }
