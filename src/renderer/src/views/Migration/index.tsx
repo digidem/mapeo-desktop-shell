03bf18dfecd0f4e294f43ad5369b0d42cc9a076c
lightlii <32508751+lightlii@users.noreply.github.com>
2023-04-25 15:37:22 +0200

    fix bg colour on migration screen

--- src/renderer/src/views/Migration/index.tsx
+++ src/renderer/src/views/Migration/index.tsx
@@ -26,7 +26,7 @@ export const MigrationView = () => {
   return (
     <OnboardingLayout>
       <SkipMigrationModal open={skipModalOpen} onClose={() => setSkipModalOpen(false)}></SkipMigrationModal>
-      <Column sx={{ bgcolor: theme.background, flex: 1 }} justifyContent={'space-between'}>
+      <Column sx={{ bgcolor: theme.white, flex: 1 }} justifyContent={'space-between'}>
         <Column spacing={3}>
           <span>
             <Typography variant="h1">{intl.formatMessage(messages.welcomeTitle)}</Typography>
