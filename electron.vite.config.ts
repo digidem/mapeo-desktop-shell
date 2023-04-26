105bcf94e9ee1f2fd6308a8c9b5f8e53f68c28d3
ErikSin <67773827+ErikSin@users.noreply.github.com>
2023-04-26 15:57:32 -0700

    added import alias

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src'),
+        '@assets': resolve('src/renderer/assets'),
       },
     },
     plugins: [react()],
