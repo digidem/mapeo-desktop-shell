0887aeb38eec881be52161633bbf5817bd7f9005
Andrew Chou <andrewchou@fastmail.com>
2023-04-27 12:43:08 -0400

    fix usage of global styles and add grayscale for svg

--- src/renderer/src/components/App/index.tsx
+++ src/renderer/src/components/App/index.tsx
@@ -6,25 +6,30 @@ import { theme, OFF_BLACK as black, OFF_WHITE as white } from '../../theme'
 import { CssBaseline, GlobalStyles } from '@mui/material'
 import { fontFace } from './fontface'
 
-const GLOBAL_STYLES = {
-  body: {
-    color: black,
-    backgroundColor: white,
-    fontFamily: `Rubik, Roboto, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif;`,
+const GLOBAL_STYLES: React.ComponentProps<typeof GlobalStyles>['styles'] = [
+  fontFace,
+  {
+    body: {
+      color: black,
+      backgroundColor: white,
+      fontFamily: `Rubik, Roboto, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', 'Segoe UI', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', sans-serif;`,
+    },
+    svg: {
+      filter: 'grayscale(1)',
+    },
   },
-  ...fontFace,
-}
+]
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <GlobalStyles styles={GLOBAL_STYLES} />
-      <IntlProvider>
-        <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles styles={GLOBAL_STYLES} />
+        <IntlProvider>
           <Router />
-        </ThemeProvider>
-      </IntlProvider>
+        </IntlProvider>
+      </ThemeProvider>
     </>
   )
 }
