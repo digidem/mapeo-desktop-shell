bb4ff4a4ad37b2e2217545da2d4d05d54d937abe
Andrew Chou <andrewchou@fastmail.com>
2023-04-27 09:32:03 -0400

    implement send invite flow (#57)

--- src/renderer/src/hooks/stores/mapeoDeviceStore.ts
+++ src/renderer/src/hooks/stores/mapeoDeviceStore.ts
@@ -29,97 +29,107 @@ type MapeoDeviceState = {
   }
 }
 
-export const useMapeoDeviceStore = create<MapeoDeviceState>()((set) => ({
-  nonMemberDevices: createRandomDevices(4, 10),
-  memberDevices: createRandomMembers(createRandomDevices(3, 5, true)),
-  projectName: 'Catapult',
-  actions: {
-    addDeviceToProject: (deviceId, role) =>
-      set((state) => {
-        if (state.memberDevices[deviceId]) {
-          throw new Error(`Device is already part of the project`)
-        }
+export const useMapeoDeviceStore = create<MapeoDeviceState>()((set) => {
+  const { members, nonMembers } = createRandomDevices(6, 15)
+  return {
+    nonMemberDevices: nonMembers,
+    memberDevices: members,
+    projectName: 'Catapult',
+    actions: {
+      addDeviceToProject: (deviceId, role) =>
+        set((state) => {
+          if (state.memberDevices[deviceId]) {
+            throw new Error(`Device is already part of the project`)
+          }
 
-        const { [deviceId]: deviceToBeAdded, ...newNonMemberDevices } = state.nonMemberDevices
+          const { [deviceId]: deviceToBeAdded, ...newNonMemberDevices } = state.nonMemberDevices
 
-        if (!deviceToBeAdded) {
-          throw new Error(`Device does not exist`)
-        }
+          if (!deviceToBeAdded) {
+            throw new Error(`Device does not exist`)
+          }
 
-        return {
-          nonMemberDevices: newNonMemberDevices,
-          memberDevices: {
-            ...state.memberDevices,
-            [deviceId]: {
-              ...deviceToBeAdded,
-              role,
-              dateAdded: Date.now(),
+          return {
+            nonMemberDevices: newNonMemberDevices,
+            memberDevices: {
+              ...state.memberDevices,
+              [deviceId]: {
+                ...deviceToBeAdded,
+                role,
+                dateAdded: Date.now(),
+              },
             },
-          },
-        }
-      }),
-    removeDeviceFromProject: (deviceId) =>
-      set((state) => {
-        const { [deviceId]: deviceToBeRemoved, ...newMemberDevices } = state.memberDevices
+          }
+        }),
+      removeDeviceFromProject: (deviceId) =>
+        set((state) => {
+          const { [deviceId]: deviceToBeRemoved, ...newMemberDevices } = state.memberDevices
 
-        if (!deviceToBeRemoved) {
-          throw new Error(`Device is not part of the project`)
-        }
+          if (!deviceToBeRemoved) {
+            throw new Error(`Device is not part of the project`)
+          }
 
-        const { dateAdded, role, ...nonMemberDevice } = deviceToBeRemoved
+          const { dateAdded, role, ...nonMemberDevice } = deviceToBeRemoved
 
-        return {
-          memberDevices: newMemberDevices,
-          nonMemberDevices: { ...state.nonMemberDevices, [deviceId]: nonMemberDevice },
-        }
-      }),
-    changeDeviceRole: (deviceId, newRole) =>
-      set((state) => {
-        const deviceToBeChanged = state.memberDevices[deviceId]
+          return {
+            memberDevices: newMemberDevices,
+            nonMemberDevices: { ...state.nonMemberDevices, [deviceId]: nonMemberDevice },
+          }
+        }),
+      changeDeviceRole: (deviceId, newRole) =>
+        set((state) => {
+          const deviceToBeChanged = state.memberDevices[deviceId]
 
-        if (!deviceToBeChanged) {
-          throw new Error(`Device is not part of the project`)
-        }
+          if (!deviceToBeChanged) {
+            throw new Error(`Device is not part of the project`)
+          }
 
-        return {
-          memberDevices: { ...state.memberDevices, [deviceId]: { ...deviceToBeChanged, role: newRole } },
-        }
-      }),
-    setProjectName: (name) => set(() => ({ projectName: name })),
-  },
-}))
+          return {
+            memberDevices: { ...state.memberDevices, [deviceId]: { ...deviceToBeChanged, role: newRole } },
+          }
+        }),
+      setProjectName: (name) => set(() => ({ projectName: name })),
+    },
+  }
+})
 
 export const useMapeoDeviceStoreAction = () => useMapeoDeviceStore((store) => store.actions)
 
-function createRandomDevices(min: number, max: number, includeSelf = false) {
+function createRandomDevices(min: number, max: number) {
   const numberOfDevices = randomInteger(min, max)
-  let mapeoDevices: Record<string, Device> = {}
 
-  for (let i = 1; i <= numberOfDevices; i++) {
-    mapeoDevices[i] = {
-      name: `Peer ${i}`,
-      deviceId: i.toString(),
-      deviceType: Math.random() > 0.6 ? 'desktop' : 'mobile',
-      isSelf: (includeSelf && i === 1) || undefined,
-    }
-  }
-
-  return mapeoDevices
-}
-
-function createRandomMembers(devices: Record<string, Device>) {
+  const nonMembers: Record<string, Device> = {}
   const members: Record<string, MemberDevice> = {}
 
-  for (const id of Object.keys(devices)) {
-    const device = devices[id]
-    members[id] = {
-      ...device,
-      role: device.isSelf || Math.random() > 0.8 ? 'coordinator' : 'participant',
-      dateAdded: Date.now(),
+  for (let i = 1; i <= numberOfDevices; i++) {
+    // Always create at least one member and one non-member device
+    const isMember = i === 1 ? true : i === 2 ? false : Math.random() > 0.6
+
+    const deviceType = Math.random() > 0.75 ? 'desktop' : 'mobile'
+
+    const deviceInfo: Device = {
+      name: `Peer ${i}`,
+      deviceType,
+      deviceId: deviceType === 'mobile' ? `Android ${i}` : `Desktop ${i}`,
+    }
+
+    if (i === 1) {
+      deviceInfo.isSelf = true
+    }
+
+    if (isMember) {
+      const memberDeviceInfo: MemberDevice = {
+        ...deviceInfo,
+        role: deviceInfo.isSelf || Math.random() > 0.8 ? 'coordinator' : 'participant',
+        dateAdded: Date.now(),
+      }
+
+      members[memberDeviceInfo.deviceId] = memberDeviceInfo
+    } else {
+      nonMembers[deviceInfo.deviceId] = deviceInfo
     }
   }
 
-  return members
+  return { members, nonMembers }
 }
 
 function randomInteger(min: number, max: number) {
